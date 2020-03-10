8c34881618b2292e8cfc235711cbfaa4baf847c5
bk185121 <bk185121@ncr.com>
2020-03-10 12:51:02 +0530

    Integrated registration page with backend api

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,6 +1,7 @@
 const AppConfig = {
     DOMAIN: "https://humming-psql.herokuapp.com",
     LOGIN: "/login",
+    REGISTER: "/register",
     GET_MARKERS:"/api/v1/getmarker",
     UPDATE_USER_DP: "/api/v1/awsupdate",
     SAVE_MARKER: "/api/v1/savemarker",
